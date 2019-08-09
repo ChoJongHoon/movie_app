ec4a442b042c2455219843fab0f69457927ebd0b
Jonghoon <huni452@naver.com>
2019-08-10 01:35:17 +0900

    Updates

---
+++ precache-manifest.148e291c8e6856dbbd95ad713f12e2ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f2f4d6ffcedd5d582510ae0dc82a52",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "78576ef80ae67c5707a4",
+    "url": "/movie_app/static/css/main.72691621.chunk.css"
+  },
+  {
+    "revision": "3cc7f05352d8eadd290c",
+    "url": "/movie_app/static/js/2.6e87735a.chunk.js"
+  },
+  {
+    "revision": "78576ef80ae67c5707a4",
+    "url": "/movie_app/static/js/main.9fde9466.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
